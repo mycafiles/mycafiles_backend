4a2b3176493efb03770cbaacdb75af1349ac2717
mycafiles <ca.maincloud@gmail.com>
2026-02-04 18:59:59 +0530

    Update fileStorage.js

--- config/fileStorage.js
+++ config/fileStorage.js
@@ -1,12 +1,15 @@
 const multer = require('multer');
 
-// Configure Multer to store files in memory as Buffers
-// This allows us to stream them to MinIO manually in the controller
-const storage = multer.memoryStorage();
-
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 50 * 1024 * 1024 } // 50MB limit
+  storage: multer.diskStorage({
+    destination: '/tmp',
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 200 * 1024 * 1024, // 200MB
+  },
 });
 
 module.exports = { upload };
